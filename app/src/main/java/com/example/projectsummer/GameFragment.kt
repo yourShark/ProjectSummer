7750c8f476f390c81eba11398c0320f04ed79d2c
Blinov Dmitrii <dimablinov203@mail.ru>
2022-07-05 16:16:09 +0300

    add project

--- app/src/main/java/com/example/projectsummer/GameFragment.kt
+++ app/src/main/java/com/example/projectsummer/GameFragment.kt
@@ -13,10 +13,15 @@ import com.example.projectsummer.databinding.FragmentGameBinding
 class GameFragment : Fragment(R.layout.fragment_game) {
 
     private var _binding: FragmentGameBinding? = null
-    lateinit var  mTimer: TextView
+    lateinit var  gameTimer: TextView
+    lateinit var gameText: TextView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        gameTimer = view.findViewById(R.id.tv_timer)
+        _binding = FragmentGameBinding.bind(view)
+        gameText = view.findViewById(R.id.tv_game_text)
 
         // Переворот экрана в горизонтальную ориентацию
         if (savedInstanceState == null) {
@@ -25,24 +30,36 @@ class GameFragment : Fragment(R.layout.fragment_game) {
             }
         }
 
-        mTimer = view.findViewById(R.id.tv_timer)
-        _binding = FragmentGameBinding.bind(view)
-
-        // Запуск таймера
-        object : CountDownTimer(10000, 1000) {
+        // Запуск таймера подготовки
+        object : CountDownTimer(4000, 1000) {
             override fun onTick(milliseconds: Long) {
                 val s: Long = milliseconds % 60000 / 1000
-
-                mTimer.text = String.format(s.toString())
+                gameText.text = String.format(s.toString())
             }
 
             override fun onFinish() {
-                mTimer.text = ""
-                lifecycleScope.launchWhenResumed {
-                    findNavController().navigate(R.id.action_gameFragment_to_resultFragment)
-                }
+
+                gameText.text = "Железный человек"
+
+                // Запуск таймера игры
+                object : CountDownTimer(10000, 1000) {
+                    override fun onTick(milliseconds: Long) {
+                        val s: Long = milliseconds % 60000 / 1000
+
+                        gameTimer.text = String.format(s.toString())
+                    }
+
+                    override fun onFinish() {
+                        gameTimer.text = ""
+                        lifecycleScope.launchWhenResumed {
+                            findNavController().navigate(R.id.action_gameFragment_to_resultFragment)
+                        }
+                    }
+                }.start()
             }
         }.start()
+
+
 
         // ПЕРЕХОД НА НОВЫЙ TopicFragment ПРИ НАЖАТИИ НА BACK
         val callback = object : OnBackPressedCallback(true) {
